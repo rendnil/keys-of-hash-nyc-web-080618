3765553f80cbcecae7c97c820b2211a8c518b315
Andrew Lindner <andrew.lindner.15@gmail.com>
2018-07-22 17:47:34 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -17,5 +17,5 @@ end
 
 #############
 animals =  {"sugar glider"=>"Australia","aye-aye"=> "Madagascar","red-footed tortoise"=>"Panama","kangaroo"=> "Australia","tomato frog"=>"Madagascar","koala"=>"Australia"} 
-puts animals.new.keys_of("Panama")
+puts animals.keys_of("Panama")
 puts animals.key("Australia")
