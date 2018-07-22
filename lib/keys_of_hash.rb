b1b5d73813d9a33ca46f82d8da2e3da2fc5a14bb
Andrew Lindner <andrew.lindner.15@gmail.com>
2018-07-22 17:39:17 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -16,5 +16,5 @@ end
 
 
 #############
-animals =  {"sugar glider"=>"Australia","aye-aye"=> "Madagascar","red-footed tortoise"=>"Panama","kangaroo"=> "Australia","tomato frog"=>"Madagascar","koala"=>"Australia"} 
-print animals.keys_of("Panama")
+#animals =  {"sugar glider"=>"Australia","aye-aye"=> "Madagascar","red-footed tortoise"=>"Panama","kangaroo"=> "Australia","tomato frog"=>"Madagascar","koala"=>"Australia"} 
+#print animals.keys_of("Panama")
